48d2685208a85b9e7e7f77c2e2e3681d2b6fde5c
liluxuan <liluxuan1997@gmail.com>
2019-01-23 07:06:00 +0000

    add decoder and modify other files to fit current architecture

--- model.py
+++ model.py
@@ -3,9 +3,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from functools import partial
-class encoder(nn.Module):
+class Encoder(nn.Module):
     def __init__(self,dim=64,n_layer=5):
-        super(encoder, self).__init__()
+        super(Encoder, self).__init__()
         # default image size :128
         d_in=3
         d_out=dim
@@ -19,7 +19,16 @@ class encoder(nn.Module):
             self.blocks.append(seq)
             d_in=d_out
             d_out*=2
+        self._weight_init()
         
+    def _weight_init(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                m.weight = nn.init.kaiming_normal_(m.weight, mode='fan_out')
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()  
+                
     def forward(self, input):
         EncoderOutputs=[]
         cur_input=input
@@ -29,48 +38,45 @@ class encoder(nn.Module):
             cur_input=enc_out
         return EncoderOutputs
     
-class decoder(nn.Module):
-    def __init__(self):
-        super(decoder, self).__init__()
-
-        self.fc1=nn.Linear(512, 256)
-        self.attribute_expand = nn.Linear(6, 256)
-
-        self.TransConv1 = nn.ConvTranspose2d(512,256,3,stride=4) # 256*4*4
-        self.TransConv2 = nn.ConvTranspose2d(256, 128, 3,stride=2) # 128*8*8
-        self.TransConv3 = nn.ConvTranspose2d(128, 64, 3,stride=2) # 64*16*16
-        self.TransConv4 = nn.ConvTranspose2d(64, 32, 3,stride=2) # 32*32*32
-        self.TransConv5 = nn.ConvTranspose2d(32, 3, 3,stride=2,output_padding=1) # 3*64*64
+class Decoder(nn.Module):
+    def __init__(self, nlayers=5, shortcut_layer=1):
+        super(Decoder, self).__init__()
         
-        self.relu = nn.ReLU()
-        self.sigmoid = nn.Sigmoid()
-
+        hidden_dim = [1064, 512, 256, 128, 64, 3]    
+        self.layers = nlayers
+        self.shortcut_layer = shortcut_layer
+        self.blocks = []
+        for i in range(nlayers-1):
+            self.blocks.append(self.block(hidden_dim[i], hidden_dim[i+1]))
+            
+        self.blocks.append(nn.Sequential(nn.ConvTranspose2d(64,3,4,2),
+                                     nn.BatchNorm2d(3),
+                                     nn.Sigmoid()))
+        self._weight_init()
         
-    def forward(self, content, attribute):                                                   
-        x = self.fc1(content)
-        a = self.attribute_expand(attribute)
-        x = torch.cat([x,a],dim=1)
-        x = x.view(-1,512,1,1)
-        
-        x = self.relu(self.TransConv1(x))
-        x = self.relu(self.TransConv2(x))
-        x = self.relu(self.TransConv3(x))
-        x = self.relu(self.TransConv4(x))
-        x = self.sigmoid(self.TransConv5(x))
-        return x
+    def _weight_init(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                m.weight = nn.init.kaiming_normal_(m.weight, mode='fan_out')
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
 
+    def block(self, input_dim, output_dim):
+        return nn.Sequential(nn.ConvTranspose2d(input_dim,output_dim,4,2),
+                                     nn.BatchNorm2d(output_dim),
+                                     nn.ReLU())
     
-class att_trans(nn.Module):
-    def __init__(self,num_person):
-        super(att_trans, self).__init__()
-        self.encoder = encoder(num_person)
-        self.decoder = decoder()
-   
-    def forward(self, img_1, img_att_2):
-        content, identity = self.encoder(img_1)
-        img2_pre = self.decoder(content, img_att_2)
-        return img2_pre, identity
-
+    def forward(self, z, attribute):  
+        batch_size, att_num = attribute.shape
+        att_expand = attribute.view(batch_size, att_num, 1, 1).expand_as(z[-1])
+        x = torch.cat([z[-1], att_expand], dim=1)
+        for i in range(self.layers):
+            x = self.blocks[i](x)
+            if i < self.shortcut_layer:
+                x = x + z[i+1]
+        return x
+    
 class Classifier(nn.Module):
     def __init__(self,num_classes=40,input_dimension=128,n_layer=5,dim=64):
         super(Classifier,self).__init__()
@@ -93,6 +99,19 @@ class Classifier(nn.Module):
         self.fc1=nn.Linear(4*4*1024,1024)
         self.fc2=nn.Linear(1024,self.num_classes)
         self.softmaxlayer = nn.Softmax(self.num_classes)
+        self._weight_init()
+        
+    def _weight_init(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                m.weight = nn.init.kaiming_normal_(m.weight, mode='fan_out')
+            elif isinstance(m, nn.Linear):
+                m.weight = nn.init.xavier_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+
     def forward(self, input):
         cur_input=input
         for i in range(self.n_layer):
---
+++ model_loss.py
@@ -0,0 +1,17 @@
+import torch
+
+def recon_loss(input, target):
+    mse_loss = nn.MSELoss()
+    mae_loss = nn.L1Loss()
+    return mse_loss(input,target) + mae_loss(input,target)
+
+def diff_loss(input, target):
+    l1_loss = nn.L1Loss()
+    if type(input) != list:
+        print('z is not a list')
+    return l1_loss(input[-1], target[-1])
+
+def classify_loss(input, target):
+    bce_loss = nn.BCELoss()
+    return bce_loss(input, target)
+ 
